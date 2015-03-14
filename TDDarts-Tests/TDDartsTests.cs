010ec32f9dd67ff052294dea1f82070bde282c8b
Edward Dewhurst <edward.dewhurst@scribestar.com>
2015-03-14 17:53:26 +0000

    initial commit

---
+++ TDDarts-Tests/TDDartsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDarts_Tests
+{
+    [TestFixture]
+    class TDDartsTests
+    {
+        [Test]
+        public void ShouldCreateMainWindow()
+        {
+            var game = new DartsGame();
+
+            Assert.That(game.window.isVIsible(), Is.True);
+        }
+    }
+}
